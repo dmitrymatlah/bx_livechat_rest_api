12182b39def4c864ab112fa38e186fc59c9b292e
dmitrymatlah <32898853+dmitrymatlah@users.noreply.github.com>
2018-03-19 13:58:11 +0300

    PetStory text change

--- src/Entities/Chat.php
+++ src/Entities/Chat.php
@@ -27,7 +27,7 @@ class Chat extends BitrixLiveChat
     private $chat = null;
 
     const SYSTEM_AVATAR = '/upload/system_avatar.png';
-    const SYSTEM_NAME = 'PetStory';
+    const SYSTEM_NAME = 'Petstory';
 
     public function __construct($chatAlias, $chatHash = '', $checkIsOpened = true)
     {
